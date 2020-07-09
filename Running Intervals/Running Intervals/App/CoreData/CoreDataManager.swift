8fb3ad109f5c8c6f8c664536508bab977a7d0f69
Shlomo Carmen <shlomoc@ravtech.co.il>
2020-07-09 09:30:38 +0300

    Added core data to save run and location

---
+++ Running Intervals/Running Intervals/App/CoreData/CoreDataManager.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataManager.swift
+//  Running Intervals
+//
+//  Created by Shlomo Carmen on 08/07/2020.
+//  Copyright © 2020 Running. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataManager {
+  
+  static let persistentContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "RunningIntervals")
+    container.loadPersistentStores { (_, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  static var context: NSManagedObjectContext { return persistentContainer.viewContext }
+  
+  class func saveContext () {
+    let context = persistentContainer.viewContext
+    
+    guard context.hasChanges else {
+      return
+    }
+    
+    do {
+      try context.save()
+    } catch {
+      let nserror = error as NSError
+      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    }
+  }
+}
