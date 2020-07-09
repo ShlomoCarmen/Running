c7d5a24970882af1dc43f57abb1229182bacc733
Shlomo Carmen <shlomoc@ravtech.co.il>
2020-07-09 13:42:41 +0300

    Completed the logic for coach run

--- Running Intervals/Running Intervals/Controller/Coach/CoachRunViewController.swift
+++ Running Intervals/Running Intervals/Controller/Coach/CoachRunViewController.swift
@@ -21,6 +21,13 @@ class CoachRunViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var doneButton: UIButton!
     
+    @IBOutlet weak var timeLeftTitleLabel: UILabel!
+    @IBOutlet weak var totalTimeLeftLabel: UILabel!
+    
+    @IBOutlet weak var trainingDoneView: UIView!
+    @IBOutlet weak var trainingDoneLabel: UILabel!
+    @IBOutlet weak var trainingDoneButton: UIButton!
+    
     var weekNumber: Int?
     var training: [Training]?
     var currentTraining: Training?
@@ -31,16 +38,24 @@ class CoachRunViewController: UIViewController {
     
     var fastSongsPersistentID: [String] = []
     var slowSongsPersistentID: [String] = []
-    
+    var isPlaying: Bool = false
     var player = MPMusicPlayerController.applicationQueuePlayer
-    
+    private var seconds = 0
+    private var timer: Timer?
+    private var totalDistance = Measurement(value: 0, unit: UnitLength.meters)
+    var locationList: [CLLocation] = []
+    let locationManager = CLLocationManager()
+    var newRun: Run?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.training = TrainingRepository.shared.getAllTraining()
         self.setText()
         self.setCornerRadius()
+        self.trainingDoneView.isHidden = true
     
+        self.locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -68,7 +83,8 @@ class CoachRunViewController: UIViewController {
     }
     
     @IBAction func startButtonPressed(_ sender: Any) {
-        
+        self.isPlaying = true
+        self.play()
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
@@ -77,5 +93,148 @@ class CoachRunViewController: UIViewController {
         }  
     }
     
+    @IBAction func trainingDoneButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: "presentStatistics", sender: self)
+    }
+    
+    func play() {
+        guard let training = self.currentTraining else { return }
+        self.playSlowSong(withDuration: 0.0)
+        let total = (training.timeForRun * 60 + training.timeForWalk * 60) * training.intervals
+        self.trainingCompleted(Double(total))
+        var counter = total
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+            self.seconds += 1
+            self.setTotalTimeLabel(time: Double(counter))
+            counter -= 1
+            if counter < 0 {
+                timer.invalidate()
+            }
+        }
+    }
+    
+    func setTotalTimeLabel(time: TimeInterval) {
+        let minutes = Int(time/60)
+        let seconds = Int(time.truncatingRemainder(dividingBy: 60))
+        let minutesString = minutes < 10 ? "0\(minutes)" : "\(minutes)"
+        let secondsString = seconds < 10 ? "0\(seconds)" : "\(seconds)"
+        self.totalTimeLeftLabel.text = "\(Strings.totalTimeLeft) \(minutesString):\(secondsString)"
+    }
+    
+    func playSlowSong(withDuration: Double) {
+        guard let training = self.currentTraining else { return }
+        self.locationManager.startUpdatingLocation()
+        DispatchQueue.main.asyncAfter(deadline: .now() + withDuration, execute: {
+            if !self.isPlaying { return }
+            self.timeLeftTitleLabel.isHidden = false
+            var counter = training.timeForWalk * 60
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+                let string = String(format:Strings.walkingTimeLeft, "\(counter)")
+                self.timeLeftTitleLabel.text = string
+                counter -= 1
+                if counter < 0 {
+                    timer.invalidate()
+                }
+            }
+            let mediaCollection = MPMediaItemCollection(items: self.walkingMediaItems)
+            self.player.setQueue(with: mediaCollection)
+            self.player.play()
+            
+            self.playFastSong(withDuration: Double(training.timeForWalk * 60))
+        })
+        
+    }
+    
+    func playFastSong(withDuration: Double) {
+        guard let training = self.currentTraining else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + withDuration, execute: {
+            if !self.isPlaying { return }
+            var counter = training.timeForRun * 60
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (timer) in
+                let string = String(format:Strings.runningTimeLeft, "\(counter)")
+                self.timeLeftTitleLabel.text = string
+                counter -= 1
+                if counter < 0 {
+                    timer.invalidate()
+                }
+            }
+            let mediaCollection = MPMediaItemCollection(items: self.runningMediaItems)
+            self.player.setQueue(with: mediaCollection)
+            self.player.play()
+            
+            self.playSlowSong(withDuration: Double(training.timeForRun * 60))
+        })
+    }
+    
+    func trainingCompleted(_ total: Double) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 20) {
+            self.startButton.isEnabled = false
+            self.startButton.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+            self.startButton.setTitle("Done", for: .normal)
+            self.timeLeftTitleLabel.isHidden = true
+            self.totalTimeLeftLabel.isHidden = true
+            self.isPlaying = false
+            self.player.stop()
+            self.locationManager.stopUpdatingLocation()
+            self.timer?.invalidate()
+            
+            if UserDefaultsProvider.shared.training <= 120 {
+                UserDefaultsProvider.shared.training += 1
+            }
+            self.trainingDone()
+        }
+    }
+    
+    func trainingDone() {
+        self.saveRun()
+        self.trainingDoneView.isHidden = false
+        self.trainingDoneLabel.text = "Congratulations, you have reached your goal"
+        self.trainingDoneButton.setTitle("Show Statistics", for: .normal)
+    }
+    
+    private func saveRun() {
+        let run = Run(context: CoreDataManager.context)
+        run.distance = totalDistance.value
+        run.duration = Int16(seconds)
+        run.timestamp = Date()
+        
+        for location in locationList {
+            let locationObject = Location(context: CoreDataManager.context)
+            locationObject.timestamp = location.timestamp
+            locationObject.latitude = location.coordinate.latitude
+            locationObject.longitude = location.coordinate.longitude
+            run.addToLocation(locationObject)
+        }
+        
+        CoreDataManager.saveContext()
+        
+        self.newRun = run
+    }
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "presentStatistics" {
+            let statisticsVC = segue.destination as! LastRunViewController
+            statisticsVC.locationList = self.locationList
+            statisticsVC.totalDistance = self.totalDistance
+            statisticsVC.run = self.newRun
+        }
+    }
+}
+
+extension CoachRunViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        for newLocation in locations {
+            let howRecent = newLocation.timestamp.timeIntervalSinceNow
+            guard newLocation.horizontalAccuracy < 20 && abs(howRecent) < 10 else { continue }
+            if let lastLocation = locationList.last {
+                let delta = newLocation.distance(from: lastLocation)
+                totalDistance = totalDistance + Measurement(value: delta, unit: UnitLength.meters)
+            }
+            locationList.append(newLocation)
+        }
+    }
     
 }
