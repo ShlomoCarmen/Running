727819186b5e857d0ef309fe6a3800033c037999
Shlomo Carmen <shlomoc@ravtech.co.il>
2020-07-20 20:17:32 +0300

    Added session id to run

--- Running Intervals/Running Intervals/App/CoreData/CoreDataManager.swift
+++ Running Intervals/Running Intervals/App/CoreData/CoreDataManager.swift
@@ -9,31 +9,33 @@
 import CoreData
 
 class CoreDataManager {
-  
-  static let persistentContainer: NSPersistentContainer = {
-    let container = NSPersistentContainer(name: "RunningIntervals")
-    container.loadPersistentStores { (_, error) in
-      if let error = error as NSError? {
-        fatalError("Unresolved error \(error), \(error.userInfo)")
-      }
-    }
-    return container
-  }()
-  
-  static var context: NSManagedObjectContext { return persistentContainer.viewContext }
-  
-  class func saveContext () {
-    let context = persistentContainer.viewContext
     
-    guard context.hasChanges else {
-      return
+    static let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "RunningIntervals")
+        container.loadPersistentStores { (_, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
     }
     
-    do {
-      try context.save()
-    } catch {
-      let nserror = error as NSError
-      fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+    class func saveContext () {
+        let context = persistentContainer.viewContext
+        
+        guard context.hasChanges else {
+            return
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
     }
-  }
 }
